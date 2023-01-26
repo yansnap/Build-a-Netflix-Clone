4f5851f2f199a21b3c091cbd39d6c9fda344002f
Yehor Datsenko <yehor.datsenko@webitel.com>
2023-01-26 14:32:42 +0200

    server side rendering

--- netflix-clone/pages/index.tsx
+++ netflix-clone/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Header from '../components/Header';
 import Banner from '../components/Banner';
+import requests from '../utils/requests';
 
 const Home: NextPage = () => {
   return (
@@ -15,8 +16,8 @@ const Home: NextPage = () => {
       <Header />
 
       <main>
-      <Banner /> 
-      <section>
+        <Banner />
+        <section>
           {/* Row */}
           {/* Row */}
           {/* Row */}
@@ -32,3 +33,37 @@ const Home: NextPage = () => {
 
 export default Home;
 
+export const getServerSideProps = async () => {
+  const [
+    netflixOriginals,
+    trendingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries,
+  ] = await Promise.all([
+    fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
+    fetch(requests.fetchTrending).then((res) => res.json()),
+    fetch(requests.fetchTopRated).then((res) => res.json()),
+    fetch(requests.fetchActionMovies).then((res) => res.json()),
+    fetch(requests.fetchComedyMovies).then((res) => res.json()),
+    fetch(requests.fetchHorrorMovies).then((res) => res.json()),
+    fetch(requests.fetchRomanceMovies).then((res) => res.json()),
+    fetch(requests.fetchDocumentaries).then((res) => res.json()),
+  ]);
+
+  return {
+    props: {
+      netflixOriginals: netflixOriginals.results,
+      trendingNow: trendingNow.results,
+      topRated: topRated.results,
+      actionMovies: actionMovies.results,
+      comedyMovies: comedyMovies.results,
+      horrorMovies: horrorMovies.results,
+      romanceMovies: romanceMovies.results,
+      documentaries: documentaries.results
+    },
+  };
+};
