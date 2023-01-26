9d84792d0ec3a78f96cf1185fabba8a0ba5b497a
Yehor Datsenko <yehor.datsenko@webitel.com>
2023-01-26 16:55:39 +0200

    created row component

--- netflix-clone/components/Row.tsx
+++ netflix-clone/components/Row.tsx
@@ -1,13 +1,40 @@
-import { Movie } from "../typing";
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid';
+import { Movie } from '../typing';
+import Thumbnail from './Thumbnail';
 
 interface Props {
-    title: string
-    movies: Movie[]
+  title: string;
+  movies: Movie[];
 }
 
-
 function Row({ title, movies }: Props) {
-  return <div>Row</div>;
+  return (
+    <div className="h-40 space-y-0.5 md:space-y-2">
+      <h2
+        className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5]
+       transition duration-200 hover:text-white md:text-2xl"
+      >
+        {title}
+      </h2>
+      <div className="group relative md:-ml-2">
+        <ChevronLeftIcon
+          className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer 
+        transition hover:scale-125 group-hover:opacity-100 opacity-0"
+        />
+
+        <div className="flex items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2">
+        {movies.map((movie) => (
+            <Thumbnail key={movie.id} movie={movie} />
+          ))}
+        </div>
+
+        <ChevronRightIcon
+          className="absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer
+         transition hover:scale-125 group-hover:opacity-100  opacity-0"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Row;
