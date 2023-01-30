a4227aa8556312e3b58683c5aba35907751e3c0d
yansnap <jan.kozhyn@gmail.com>
2023-01-30 18:48:36 +0200

    create from on login page

--- netflix-clone/pages/login.tsx
+++ netflix-clone/pages/login.tsx
@@ -1,7 +1,51 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className="relative flex h-screen flec=col bg-black md:items-center md:justify-center md:bg-transparent">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Image
+        src="https://rb.gy/p2hphi"
+        layout="fill"
+        alt="image"
+        className="-z-10 !hidden opacity-60 sm:!inline"
+        objectFit="cover"
+      />
+      <img
+        src="https://rb.gy/ulxxee"
+        className="absolute left-4 top-4 cursor-pointer object-contain md:left-10 md:top-6"
+        width={150}
+        height={150}
+      />
+
+      <form className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14">
+        <h1 className="text-4xl font-semibold">Sign in</h1>
+        <div className="space-y-4">
+          <label className="inline-block w-full">
+            <input type="email" placeholder="Email" className="input" />
+          </label>
+          <label className="inline-block w-full">
+            <input type="password" placeholder="Password" className="input" />
+          </label>
+        </div>
+
+        <button className="w-full rounded bg-[#e50914] py-3 font-semibold">
+          Sign In
+        </button>
+
+        <div className="text-[gray] ">
+          New to Netflix?{' '}
+          <button type="submit" className="text-white hover:text-zinc-300 ">
+            Sign up now
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
